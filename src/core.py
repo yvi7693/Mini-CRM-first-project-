2d947e6895cdb5763620b87c2efc176db52d0aaf
Yaroslav Volkov <yvi_7693@icloud.com>
2025-12-28 10:14:37 +0300

    Добавил обработку команд в функции main_loop

--- src/core.py
+++ src/core.py
@@ -1,4 +1,4 @@
-
+from basic_logic import *
 
 def start():
     pass
@@ -6,7 +6,29 @@ def start():
 def main_loop():
     command = input("Введите команду >>")
 
+    if command == "1":
+        add_client()
 
+    elif command == "2":
+        watch_all_clients()
+
+    elif command == "3":
+        edit_client()
+
+    elif command == "4":
+        delete_client()
+
+    elif command == "5":
+        search_client()
+
+    elif command == "6":
+        filtering_clients()
+
+    elif command == "7":
+        sort_client()
+
+    elif command == "8":
+        find_statistic()
 
 def stop():
     pass

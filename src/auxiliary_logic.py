524c26cf7a8e0ef3cd3b6e56e43944163d99b295
Yaroslav Volkov <yvi_7693@icloud.com>
2025-12-29 13:13:14 +0300

    Добавил функцию validate_number_phone

--- src/auxiliary_logic.py
+++ src/auxiliary_logic.py
@@ -7,12 +7,23 @@ def validate_name(name: str) -> bool:
 
     return True
 
-def validate_age(age: str) -> bool:
-    if age.isdigit() == False:
+def validate_age(number: str) -> bool:
+    if not number.isdigit():
         return False
 
-    if int(age) < 0 or int(age) > 200:
+    if int(number) < 0 or int(number) > 200:
         return False
 
     return True
+
+
+def validate_number_phone(number_phone: str) -> bool:
+    if not number_phone.isdigit():
+        return False
+
+    if len(number_phone) != 11:
+        return False
+
+    return True
+
 

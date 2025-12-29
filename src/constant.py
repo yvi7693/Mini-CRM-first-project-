f2427cbc0b2d4614e41efc5f91931634accb5845
Yaroslav Volkov <yvi_7693@icloud.com>
2025-12-29 12:13:26 +0300

    Добавил константы команд

--- src/constant.py
+++ src/constant.py
@@ -1,1 +1,12 @@
+from pickle import ADDITEMS
+
 PATH_CLIENT = "client.json"
+
+ADD_COMMAND = "add"
+LIST_COMMAND = "list"
+EDIT_COMMAND = "edit"
+DELETE_COMMAND = "delete"
+SEARCH_COMMAND = "search"
+FILTER_COMMAND = "filter"
+SORT_COMMAND = "sort"
+STAT_COMMAND = "stat"

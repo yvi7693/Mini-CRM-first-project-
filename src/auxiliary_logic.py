441c349f857ca280a14355ce924c71e8cc25c4ac
Yaroslav Volkov <yvi_7693@icloud.com>
2025-12-29 13:01:40 +0300

    Добавил функцию validate_age

--- src/auxiliary_logic.py
+++ src/auxiliary_logic.py
@@ -7,3 +7,12 @@ def validate_name(name: str) -> bool:
 
     return True
 
+def validate_age(age: str) -> bool:
+    if age.isdigit() == False:
+        return False
+
+    if int(age) < 0 or int(age) > 200:
+        return False
+
+    return True
+

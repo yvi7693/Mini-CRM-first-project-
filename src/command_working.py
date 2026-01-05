a612db38945b3e9f11cd64f492507029986f681f
Yaroslav Volkov <yvi_7693@icloud.com>
2026-01-05 15:09:37 +0300

    Добавлена функция processing_edit_client

--- src/command_working.py
+++ src/command_working.py
@@ -35,3 +35,39 @@ def processing_list_client() -> None:
     show_list_client(list_client)
 
     return None
+
+
+def processing_edit_client() -> None:
+    show_input_message("Введите id клиента данные которого хотели бы отредактировать >> ")
+    client_id = input()
+
+    if not validate_id(client_id):
+        show_error_message("Ввденный id не корректен")
+
+    else:
+
+        show_input_message("Введите название компании >> ")
+        name = input()
+
+        show_input_message("Введите год основания компании >> ")
+        founded = input()
+
+        show_input_message("Введите номер телефона клиента начиная c '8' >> ")
+        number_phone = input()
+
+        show_input_message("Введите почту клиента >> ")
+        email = input()
+
+        if validate_client(name, founded, number_phone, email):
+            edit_client(PATH_CLIENT, int(client_id), name, founded, number_phone, email)
+            show_info_message("Данные клиента успешно отредактированы :)")
+
+
+        else:
+
+            show_error_message("Не корректный ввод, попробуйте еще раз!!!")
+
+    return None
+
+
+

8b11736d8c8da22dfe67659e5009753ba961a719
Yaroslav Volkov <yvi_7693@icloud.com>
2026-01-05 15:07:06 +0300

    Добавлена функция processing_add_client

---
+++ src/command_working.py
@@ -0,0 +1,31 @@
+from src.display import *
+from src.basic_logic import *
+
+
+def processing_add_client() -> None:
+    show_input_message("Введите название компании >> ")
+    name = input()
+
+    show_input_message("Введите год основания компании >> ")
+    founded = input()
+
+    show_input_message("Введите номер телефона клиента начиная c '8' >> ")
+    number_phone = input()
+
+    show_input_message("Введите почту клиента >> ")
+    email = input()
+
+    if validate_client(name, founded, number_phone, email):
+
+        add_client(name, founded, number_phone, email)
+        show_info_message("Клиент успешно добавлен :)")
+
+
+    else:
+
+        show_error_message("Не корректный ввод, попробуйте еще раз!!!")
+
+
+
+
+
